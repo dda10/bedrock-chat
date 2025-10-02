3aa9f30744e824efff9f5641413df96b4e2ba348
dda10 <ddaduyanh123@gmail.com>
2025-10-02 17:09:30 +0700

    fix pwd

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-cd frontend
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR/frontend"
+
 npm install
 npm run build
 aws s3 sync dist/ s3://${FRONTEND_BUCKET}/ --delete
